61e3e110ff7f05caf5490df33d0a8a4a1cb15b52
Khuram Niaz <knshaikh@gmail.com>
2018-02-25 01:38:14 -0700

    SearchBar searchTerm state

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -3,11 +3,26 @@ import React, { Component } from 'react'
 class SearchBar extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            searchTerm: ""
+        };
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+    onInputChange(e) {
+        this.setState({
+            searchTerm: e.target.value
+        });
     }
     render() {
         return (
             <form className="input-group">
-                <input type="text" />
+                <input
+                    type="text"
+                    placeholder="Search a city here..."
+                    className="form-control"
+                    value={this.state.searchTerm}
+                    onChange={this.onInputChange}
+                />
                 <span className="input-group-btn">
                     <button className="btn btn-secondary" type="submit">Submit</button>
                 </span>
